6ffb5c02a86d4dfb52536ffe767c921c86ed722b
Evan Kerr <evankrkerr90@gmail.com>
2018-07-20 22:18:37 +0000

    added half stars for average review ratings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,19 @@
 module ApplicationHelper
-
     def render_stars(value)
-        output = ''
-        if (1..5).include?(value.to_i)
-          value.to_i.times { output += '*'}
-        end
-        output
+        half = 0
+      output = ''
+      if (1..5).include?(value.floor)
+        value.floor.times { output += '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>'}
+      end
+      if value.to_f % 1 != 0
+        @half = 1
+        output += '<span class="glyphicon glyphicon-star half" aria-hidden="true"></span>'
+      end
+      if value.round + half <= 4
+        n = 5 - value.round + half
+        n.times {output += '<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>'}
+      end
+      output.html_safe
     end
-    
 
-
-end
+  end
